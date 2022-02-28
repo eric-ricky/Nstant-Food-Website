0bd8c6ecc6c66a0c5d2ab6702dde43829b0d2dfc
Eric Ricky <ericricky200@gmail.com>
2022-02-28 11:09:05 +0300

    troubleshoot mailchimp

--- pages/api/subscribe.js
+++ pages/api/subscribe.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const getRequestParams = (email) => {
+  // get env variables
   const API_KEY = process.env.MAILCHIMP_API_KEY;
   const LIST_ID = process.env.MAILCHIMP_LIST_ID;
-
   // mailchimp datacenter - mailchimp api keys always look like this:
   // fe4f064432e4684878063s83121e4971-us6
   // We need the us6 part
@@ -20,7 +20,6 @@ const getRequestParams = (email) => {
 
   // Api key needs to be encoded in base 64 format
   const base64ApiKey = Buffer.from(`anystring:${API_KEY}`).toString("base64");
-
   const headers = {
     "Content-Type": "application/json",
     Authorization: `Basic ${base64ApiKey}`,
@@ -44,8 +43,10 @@ const send = async (req, res) => {
 
   try {
     const { url, data, headers } = getRequestParams(email);
+
     const response = await axios.post(url, data, { headers });
 
+    // Success
     return res.status(201).json({ error: null });
   } catch (error) {
     return res.status(400).json({
