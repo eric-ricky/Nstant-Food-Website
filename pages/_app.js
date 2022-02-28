f0f066cb138b6bfa26fd2f15b3aa7b204391c85e
Eric Ricky <ericricky200@gmail.com>
2022-02-28 16:12:53 +0300

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,28 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-2B6KSDLLS`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-2B6KSDLLS3');
+                `}
+      </Script>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
